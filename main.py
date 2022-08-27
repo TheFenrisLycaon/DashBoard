4e806494d3072eabab438ef8d9ad7dc0437ffa65
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-08-27 15:04:56 +0530

    Added basic Divs and Buttons

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from dash import Dash, html
 from dash_bootstrap_components.themes import BOOTSTRAP
 
 from src.components.layouts import create_layout
+from src.data.loader import load_transactions
 
+PATH = './Data/transactions.csv'
 
 def main() -> None:
     """Main Function"""
+    data = load_transactions(PATH)
     app = Dash(external_stylesheets=[BOOTSTRAP])
     app.title = "Financial Dashboard"
     app.layout = create_layout(app)
--- src/components/layouts.py
+++ src/components/layouts.py
@@ -1,12 +1,17 @@
 from dash import Dash, html
 
+from . import year_dropdown
+
 
 def create_layout(app: Dash) -> html.Div:
 
     return html.Div(
         className="app-div",
         children=[
-            html.H1("Finances"),
+            html.H1(app.title),
             html.Hr(),
+            html.Div(
+                className="dropdown-container", children=[year_dropdown.render(app)]
+            ),
         ],
     )
